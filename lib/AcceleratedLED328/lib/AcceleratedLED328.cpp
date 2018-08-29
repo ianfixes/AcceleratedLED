19259634b75795f9425d6e97a66ab3c177697786
Benjamin Guest <benguest@gmail.com>
2018-08-28 23:21:40 -0400

    Get Acclerometer working

--- lib/AcceleratedLED328/lib/AcceleratedLED328.cpp
+++ lib/AcceleratedLED328/lib/AcceleratedLED328.cpp
@@ -13,21 +13,22 @@ void AcceleratedLED328::init(){
   blinkers.init();
 
   //accelerometer.setRange(ADXL362_4G);
-  //accelerometer.begin(10);
-  //accelerometer.beginMeasure();
+  accelerometer.begin(10);
+  accelerometer.beginMeasure();
 }
 
 void AcceleratedLED328::run(){
-  //accelerometer.readXYZTData(
-      //data.acceleration.x,
-      //data.acceleration.y,
-      //data.acceleration.y,
-      //data.temperature);
+  accelerometer.readXYZTData(
+      data.acceleration.x,
+      data.acceleration.y,
+      data.acceleration.z,
+      data.temperature);
 
 #ifdef PRINT_ACCELEROMETER
   Serial.print(data.acceleration.x); Serial.print(" ");
   Serial.print(data.acceleration.y); Serial.print(" ");
   Serial.print(data.acceleration.z); Serial.print(" ");
+  Serial.print(data.temperature); Serial.print(" ");
   Serial.print("\n");
 #endif
 
